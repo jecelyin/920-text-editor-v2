808050a08af74480690a8b866443577d20a3d5e7
jecelyin <jecelyin@gmail.com>
2017-08-23 13:49:10 +0800

    upgrade InputConnectionHacker.java

--- app/src/main/java/com/jecelyin/editor/v2/widget/text/InputConnectionHacker.java
+++ app/src/main/java/com/jecelyin/editor/v2/widget/text/InputConnectionHacker.java
@@ -18,14 +18,19 @@
 
 package com.jecelyin.editor.v2.widget.text;
 
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.KeyEvent;
 import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.CorrectionInfo;
 import android.view.inputmethod.ExtractedText;
 import android.view.inputmethod.ExtractedTextRequest;
 import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputContentInfo;
 
 /**
  * @author Jecelyin Peng <jecelyin@gmail.com>
@@ -196,5 +201,29 @@ public class InputConnectionHacker implements InputConnection {
 
     public boolean isCtrlPressed() {
         return isCtrlPressed;
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean deleteSurroundingTextInCodePoints(int beforeLength, int afterLength) {
+        return ic.deleteSurroundingTextInCodePoints(beforeLength, afterLength);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public Handler getHandler() {
+        return ic.getHandler();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public void closeConnection() {
+        ic.closeConnection();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N_MR1)
+    @Override
+    public boolean commitContent(@NonNull InputContentInfo inputContentInfo, int flags, @Nullable Bundle opts) {
+        return ic.commitContent(inputContentInfo, flags, opts);
     }
 }
